fea89af1fbad007eedb6d4b09f014b06183607b2
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2020-04-15 01:56:39 +0530

    Added new employee and employee validation

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,16 +1,46 @@
 class EmployeesController < ApplicationController
   
+  def index
+
+  end
+
   def new
-  	
+  	@employee = Employee.new
   end
 
   def create
+  	@employee = Employee.new(emp_params)
+  	if params.has_key?(:dob)
+  		if (Date.parse(params[:dob]) < 18.years.ago)
+			if @employee.save
+				flash[:success] = "Employee was added successfully"
+				redirect_to employee_path(@employee)
+			else
+				render 'new'
+			end
+		else
+			notice[:danger] = "Employees have to be atleast 18 years of age"
+		end
+	else
+		@employee.save
+		render 'new'
+	end
   end
 
   def edit
   end
 
   def update
+  end
+
+  def show
+
+  end
+
+  private
+
+  def emp_params
+  	params.require(:employee).permit(:fName, :lName, :phone, :email, :dob, :address)
   end
 
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,13 @@
 class Employee < ApplicationRecord
 	has_many :orders, :dependent => :destroy
 	has_one :user, :dependent => :destroy
+	before_save { self.email = email.downcase }
+	validates :fName, presence: true, length: { maximum: 20, minimum: 3 }
+	validates :lName, presence: true, length: { maximum: 20, minimum: 3 }
+	validates :address, presence: true, length: { maximum: 100, minimum: 5 }
+	validates :dob, presence: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 105 },	uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+	VALID_PHONE_REGEX = /\d/
+	validates :phone, presence: true, length: { maximum: 10, minimum: 10}, uniqueness: true, format: { with: VALID_PHONE_REGEX }
 end
